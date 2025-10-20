80f92f58b71aa4661566afd69703c036b9fba21f
SylviaRemington <sylviaremington@gmail.com>
2025-10-20 02:54:46 -0700

    changed order of register and login on navbar in NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -35,11 +35,12 @@ const NavBar = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
+            <Link to="/register">Register</Link>
+          </li>          
+          <li>
             <Link to="/login">Login</Link>
           </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
+          
         </ul>
       )}
     </nav>
